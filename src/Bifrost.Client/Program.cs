6ae39653ba7e072ea8e579558a9a5373344e1845
Khaos <khaos@khaos-coders.org>
2023-12-15 11:03:29 +0100

    Added HttpClient to access APIs

--- src/Bifrost.Client/Program.cs
+++ src/Bifrost.Client/Program.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
