8886a7d6f477945ccc64e4d8bfc43d58ae7518f7
Khaos <khaos@khaos-coders.org>
2023-12-27 23:21:57 +0100

    Finished first iteration of login page

---
+++ src/Bifrost.Client/Utils/BootstrapValidationClassProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Bifrost.Client.Utils;
+
+public class BootstrapValidationClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(EditContext editContext,
+        in FieldIdentifier fieldIdentifier)
+    {
+        var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+        return isValid ? "" : "is-invalid";
+    }
+}
