ee4c1c454874ed2ec3a05c085d52acf1868569a8
Khaos <khaos@khaos-coders.org>
2024-01-05 21:48:11 +0100

    Added RepositoryBase

---
+++ src/Bifrost/Data/Base/IEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Bifrost.Data.Base;
+
+/// <summary>
+/// A database entity.
+/// </summary>
+public interface IEntity
+{
+    string Id { get; set; }
+}
---
+++ src/Bifrost/Data/Base/RepositoryBase.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bifrost.Data.Base;
+
+/// <summary>
+/// Base class for all repositories.
+/// </summary>
+/// <typeparam name="TEntity">Type of the repository</typeparam>
+/// <param name="dbContext">EntityFramework DbContext</param>
+public abstract class RepositoryBase<TEntity>(DbContext dbContext) where TEntity : class, IEntity
+{
+    private readonly DbContext dbContext = dbContext;
+
+    /// <summary>
+    /// Returns all entities of the repository.
+    /// </summary>
+    /// <remarks>
+    /// No EntityFramework tracking is used.
+    /// </remarks>
+    /// <returns>Queryable list of entities</returns>
+    public IQueryable<TEntity> QueryAll() =>
+        dbContext.Set<TEntity>().AsNoTracking();
+
+    /// <summary>
+    /// Returns a single entity by its id.
+    /// </summary>
+    /// <remarks>
+    /// No EntityFramework tracking is used.
+    /// </remarks>
+    /// <param name="id">Id of entity</param>
+    /// <returns>Found entity or null</returns>
+    public Task<TEntity?> GetByIdAsync(string id) =>
+        QueryAll().FirstOrDefaultAsync(e => e.Id == id);
+
+    /// <summary>
+    /// Creates a new entity.
+    /// </summary>
+    /// <param name="entity">New entity</param>
+    /// <param name="isBulkOperation">Indicates if this is a bulk operation, so no SaveChanges is called</param>
+    /// <returns>
+    /// Awaitable task.
+    /// </returns>
+    public async Task CreateAsync(TEntity entity, bool isBulkOperation = false)
+    {
+        await dbContext.Set<TEntity>().AddAsync(entity);
+        if (isBulkOperation) return;
+        await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Updates an existing entity.
+    /// </summary>
+    /// <param name="entity">Entity to update</param>
+    /// <param name="isBulkOperation">Indicates if this is a bulk operation, so no SaveChanges is called</param>
+    /// <returns>
+    /// Awaitable task.
+    /// </returns>
+    public async Task UpdateAsync(TEntity entity, bool isBulkOperation = false)
+    {
+        dbContext.Set<TEntity>().Update(entity);
+        if (isBulkOperation) return;
+        await dbContext.SaveChangesAsync();
+    }
+
+    /// <summary>
+    /// Deletes an entity by its id.
+    /// </summary>
+    /// <param name="id">Id of entity to delete</param>
+    /// <param name="isBulkOperation">Indicates if this is a bulk operation, so no SaveChanges is called</param>
+    /// <returns>
+    /// Awaitable task.
+    /// </returns>
+    /// <exception cref="EntityNotFoundException"></exception>
+    public async Task DeleteAsync(string id, bool isBulkOperation = false)
+    {
+        var entity = await GetByIdAsync(id) ?? throw new EntityNotFoundException();
+        dbContext.Set<TEntity>().Remove(entity);
+        if (isBulkOperation) return;
+        await dbContext.SaveChangesAsync();
+    }
+}
